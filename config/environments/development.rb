76a78f42aaa708d0456aac64edabb690f08e2629
doanhongha <doanhong.ha@monstar-lab.com>
2017-11-29 13:53:23 +0900

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.exceptions_app = self.routes
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
